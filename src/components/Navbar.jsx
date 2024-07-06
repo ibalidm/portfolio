2d8f40502ab151562203303f8079f761419f881a
ibalidm <ia4520002@gmail.com>
2024-07-06 21:27:32 +0600

    feat: design navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import { GoDownload } from "react-icons/go";
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between p-5 sticky top-0 text-TextPrimary border border-red-600 font-semibold leading-5 tracking-wider'> 
+    <nav className='flex justify-between items-center p-5 sticky top-0 text-TextPrimary font-semibold leading-5 tracking-wider'> 
         <ul className='flex basis-[40%]'>
             <li className='cursor-pointer text-[20px] font-bold'>Ibrahim</li>
         </ul>
-        <ul className='hidden lg:flex basis-[40%] items-center gap-8 text-[16px]'>
+        <ul className='hidden lg:flex basis-[40%] items-center gap-14 text-[16px]'>
             <li className='cursor-pointer'>Skills</li>
             <li className='cursor-pointer'>Experience</li>
             <li className='cursor-pointer'>Education</li>
             <li className='cursor-pointer'>Achivement</li>
         </ul>
         <ul className='hidden lg:flex'>
-            <li className='cursor-pointer'>Download CV</li>
+            <button className="flex justify-center items-center border w-[200px] h-[40px] border-black rounded-[4px] hover:bg-TextSecondary hover:text-white hover:border-none transition-colors">
+                <li className='flex justify-center items-center gap-2 cursor-pointer'>
+                    <GoDownload className="text-[18px]"/>
+                    <span className="text-[15px]">Download CV</span>
+                </li>
+            </button>
         </ul>
         <ul className='flex lg:hidden'>
             Mobile menu
